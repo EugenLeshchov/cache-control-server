a82811989938f5fb198268fd353e80891ca28c06
eugenleshchov <eugenleshchov@gmail.com>
2017-01-29 21:08:42 +0300

    CRP/optimization: add files encoding support

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-const express = require('express')
-    , http = require('http')
-    , path = require('path');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const zlib = require('zlib');
+const fs = require('fs');
 
 const app = express();
 
 app.set('port', (process.env.PORT || 8080));
 
 app.get('/:name', function (req, res) {
-    if (req.params.name == null) {
-        res.send('Hi, Dude!');
-    }
-    res.set({
-        'Cache-Control': 'public, max-age=7200000',
-        'Expires': new Date(Date.now() + 7200000000)
+    const filePath = path.join(__dirname, 'files', req.params.name);
+
+    let acceptEncoding = req.headers['accept-encoding'];
+    if (!acceptEncoding) acceptEncoding = '';
+
+    fs.exists(filePath, (exists) => {
+        if (exists) {
+            res.set({
+                'Cache-Control': 'public, max-age=7200000',
+                'Expires': new Date(Date.now() + 7200000000)
+            });
+            let fileStream = fs.createReadStream(filePath);
+            if (acceptEncoding.match(/\bdeflate\b/)) {
+                res.status(200)
+                    .set({ 'content-encoding': 'deflate' });
+                fileStream.pipe(zlib.createDeflate()).pipe(res);
+            } else if (acceptEncoding.match(/\bgzip\b/)) {
+                res.status(200)
+                    .set({ 'content-encoding': 'gzip' });
+                fileStream.pipe(zlib.createGzip()).pipe(res);
+            } else {
+                res.status(200, {});
+                fileStream.pipe(res);
+            }
+        } else {
+            res.status(404).end();
+        }
     });
-    res.sendFile(path.join(__dirname, 'files', req.params.name));
 });
 
 app.listen(app.get('port'), function () {
